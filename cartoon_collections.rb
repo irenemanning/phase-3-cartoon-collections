28a85f03bf98246cac9803e936a62dbf5720bfd6
Irene <irenemanningm@gmail.com>
2022-06-21 22:45:36 -0500

    commit tree a3638fe2f84eb74c0f4936ff9d37a5600b01b7c0

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,36 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require "pry"
+def roll_call_dwarves dwarfs
+  dwarfs.map.with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet planeteer_calls
+  planeteer_calls.map do |element|
+    element.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls calls
+  length_check = calls.map do |call|
+    if call.length > 4
+      true
+    end
+  end
+  if length_check.include?(true)
+    true
+  else false
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese ingredients
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.map do |cheese|
+    single_out_cheese = ingredients.map do |ingredient|
+      if ingredient == cheese
+        return cheese
+      end
+    end
+  end
+  return nil
 end
